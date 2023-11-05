7bf818ecad586d9935ce5f0f9486c0554ded152d
yash9662 <31590769+yash9662@users.noreply.github.com>
2023-11-05 22:21:04 +0530

    Add files via upload

--- articles/views.py
+++ articles/views.py
@@ -1,14 +1,14 @@
-from django.shortcuts import render, HttpResponse, get_object_or_404, redirect
+from django.shortcuts import render, HttpResponse, get_object_or_404,redirect
 from .models import Article
-from .forms import LoginForm, UserRegistraction,ArticleRegistracionForm, ArticleUpdateForm
+from .forms import LoginForm, UserRegistration,ArticleRegistrationForm, ArticleUpdateForm
 from django.contrib.auth import authenticate, login
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.decorators import login_required
 
-
 # Create your views here.
+
+
 def article_list(request):
-    #article = 'This is my first article title'
     article_list = Article.objects.all().order_by('-published')
 
     paginator = Paginator(article_list, 3)
@@ -16,58 +16,82 @@ def article_list(request):
 
     try:
         articles = paginator.page(page)
-    
+
     except PageNotAnInteger:
         articles = paginator.page(1)
 
     except EmptyPage:
         articles = paginator.page(paginator.num_pages)
 
-    return render(request, 'articles.html', {'article_list':article_list, 'page':page})
+    return render(request, 'articles.html', {'article_list':articles, 'page':page})
+
 
 def article_details(request, slug):
     article = get_object_or_404(Article, slug=slug)
     return render(request, 'details.html', {'article':article})
 
 def user_login(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = LoginForm(request.POST)
+
         if form.is_valid():
             cd = form.cleaned_data
             user = authenticate(request, username = cd['username'], password = cd['password'])
+
             if user is not None:
                 login(request, user)
-                return HttpResponse("You are Authentacited")
-        else:
-            return HttpResponse("Invalid Login")
+                return HttpResponse("You are authenticated")
+
+
+            else:
+                return HttpResponse("Invalid Login")
+
+
     else:
         form = LoginForm()
+
     return render(request, 'account/login.html', {'form':form})
 
+
 def register(request):
-    if request.method == 'POST':
-        user_form = UserRegistraction(request.POST)
+    if request.method == "POST":
+        user_form = UserRegistration(request.POST)
+
         if user_form.is_valid():
+
             new_user = user_form.save(commit=False)
+
             new_user.set_password(user_form.cleaned_data['password'])
+
             new_user.save()
-            return render(request, 'account/register_done.html', {'user_form':user_form})
+            return render(request,'account/register_done.html', {'user_form':user_form})
+
     else:
-        user_form = UserRegistraction()
-    return render(request, 'account/register.html', {'user_form':user_form})
+        user_form = UserRegistration()
+
+    return render(request, 'account/register.html', {'user_form': user_form})
+
+
+
 
 @login_required
 def article_form(request):
-    if request.method == 'POST':
-        article_form = ArticleRegistracionForm(request.POST)        
+
+    if request.method == "POST":
+        article_form = ArticleRegistrationForm(request.POST)
+
         if article_form.is_valid():
             article = article_form.save(commit=False)
             article.author = request.user
             article.save()
             return redirect('article_list')
+
     else:
-        article_form = ArticleRegistracionForm()
+        article_form = ArticleRegistrationForm()
+
     return render(request, 'account/add_article.html', {'article_form':article_form})
+
+
 
 def update_article(request, slug):
     article = get_object_or_404(Article, slug=slug)
@@ -78,7 +102,9 @@ def update_article(request, slug):
         form.save()
         return redirect('article_list')
 
+
     return render(request, 'account/update.html', {'form':form})
+
 
 
 def delete_article(request, slug):
