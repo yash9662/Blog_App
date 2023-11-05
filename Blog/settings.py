fe97a7a0d3f48fe3582adc763f2d140642452301
yash9662 <31590769+yash9662@users.noreply.github.com>
2023-11-05 15:13:13 +0530

    Update settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-)v$x^y$8gb8gh0h52!rnu01%uf%9fvr77b7-q4@udjbo+nv+u)"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
